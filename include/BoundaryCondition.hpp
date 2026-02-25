be5357dc35727dcbb2ed22dab5f6dba686b16473
KevinCephise <kevincephise@gmail.com>
2026-02-25 23:20:03 +0100

    minor corrections

--- include/BoundaryCondition.hpp
+++ include/BoundaryCondition.hpp
@@ -6,13 +6,13 @@
 
 class BoundaryCondition{
     protected:
-    Side side; // I have to define in another file the enum Side.
+        Side side; // I have to define in another file the enum Side.
     
 
     public:
-    BoundaryCondition(Side side); // Constructor
-    virtual ~BoundaryCondition()= default; // Virtual Destructor
-    virtual void apply(std::vector<float> &T, int nx, int ny)=0; // pure virtual function
-    Side getSide() const { return side; } 
+        BoundaryCondition(Side side); // Constructor
+        virtual ~BoundaryCondition()= default; // Virtual Destructor
+        virtual void apply(std::vector<float> &T, int nx, int ny)=0; // pure virtual function
+        Side getSide() const { return side; } 
 
 };
