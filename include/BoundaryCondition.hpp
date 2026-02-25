e095002fe9c410ee85a2153125133548af7e1c08
Wassim HALIMI <wassim@MacBook-Air-de-Wassim.local>
2026-02-25 12:50:57 +0100

    Add BoundaryCondition and Side

---
+++ include/BoundaryCondition.hpp
@@ -0,0 +1,18 @@
+#pragma once // To avoid having multiples inclusions
+#include <iostream>
+#include <vector>
+#include "Side.hpp"
+
+
+class BoundaryCondition{
+    protected:
+    Side side; // I have to define in another file the enum Side.
+    
+
+    public:
+    BoundaryCondition(Side side); // Constructor
+    virtual ~BoundaryCondition()= default; // Virtual Destructor
+    virtual void apply(std::vector<float> &T, std::int nx, std::int ny)=0; // pure virtual function
+    Side getSide() const { return side; } 
+
+};
---
+++ include/Side.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+enum class Side{
+    North,
+    South,
+    East,
+    West,
+}; // We need to implement later in 3D because we'll have 6 sides.
