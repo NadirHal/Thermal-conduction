c606211a8a72ed0a5418c722bf4e8a734b9a8c8e
Wassim HALIMI <wassim@MacBook-Air-de-Wassim.local>
2026-03-11 15:13:56 +0100

    test

--- include/BoundaryCondition.hpp
+++ include/BoundaryCondition.hpp
@@ -9,7 +9,11 @@ class BoundaryCondition{
     
 
     public:
+<<<<<<< Updated upstream
         BoundaryCondition(Side side); // Constructor
+=======
+        BoundaryCondition(Side s):side(s){}; // Constructor + inline definition
+>>>>>>> Stashed changes
         virtual ~BoundaryCondition()= default; // Virtual Destructor
         virtual void apply(std::vector<float> &T, int nx, int ny)=0; // pure virtual function
         Side getSide() const { return side; } 
---
+++ include/DirichletBC.hpp
@@ -0,0 +1,19 @@
+#pragma once // To avoid having multiples inclusions
+#include <iostream>
+#include "BoundaryCondition.hpp"
+
+class DirichletBC: public BoundaryCondition {
+    private:
+    float value; 
+
+    public:
+    DirichletBC(Side s,float v): BoundaryCondition(s), value(v){}; // Constructor + inline definition 
+
+    void apply(std::vector<float> &T, int nx, int ny) override { // Redefining the function that's in the mother class. Override bc we already define it
+        //Apply Dirichlet Boundary Condition
+        
+
+
+    }
+
+};
